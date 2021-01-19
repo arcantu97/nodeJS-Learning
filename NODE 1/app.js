df27b5e5952f33b546510c3c74871c3b8903d7f4
arcantu97 <arcantu97@gmail.com>
2021-01-18 22:50:56 -0600

    First project

---
+++ NODE 1/app.js
@@ -0,0 +1,33 @@
+const argv = require('./config/yargs').argv;
+const todo = require('./todo/todo');
+const colors = require('colors');
+
+let command = argv._[0];
+let id = argv._[1];
+
+switch (command) {
+    case 'create':
+        let task = todo.create(argv.description)
+        break;
+    case 'delete':
+        console.log('Delete task');
+        break;
+    case 'update':
+        console.log('Updating task...');
+        break;
+    case 'show':
+        let tasks = todo.readTasks();
+        for (let task of tasks) {
+            console.log('Task #', `${task.id}`.green);
+            console.log('Description:', `${task.description}`.green);
+            if (task.completed == false) {
+                console.log('Status:', 'Not ready'.red);
+            } else {
+                console.log('Status:', 'Complete'.yellow);
+            }
+        }
+        break;
+    default:
+        console.log('Command not recognized.');
+        break;
+}
